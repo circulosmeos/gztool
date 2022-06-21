d47c3b8f78c26ae80b3e935c65a4097d4b7dd413
circulosmeos <roberto.s.galende@gmail.com>
2022-06-21 20:42:24 +0200

    changed log message for EXIT_FILE_OVERWRITTEN to VERBOSITY_NORMAL

--- gztool.c
+++ gztool.c
@@ -6084,7 +6084,7 @@ int main(int argc, char **argv)
                             compression_factor );
                          if ( 0 == write_index_to_disk &&
                               EXIT_FILE_OVERWRITTEN == ret_value ) {
-                              printToStderr( VERBOSITY_EXCESSIVE, "File overwriting detected and restarting decompression...\n" );
+                              printToStderr( VERBOSITY_NORMAL, "File overwriting detected and restarting decompression...\n" );
                             }
                     } while ( 0 == write_index_to_disk  &&
                               EXIT_FILE_OVERWRITTEN == ret_value );
